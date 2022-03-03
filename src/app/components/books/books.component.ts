64681fece844b38698cfe6150bd5b3837216f2f6
Cristopher <cristopher.emodesto@gmail.com>
2022-03-02 21:34:26 -0600

    Added pagination with backend

--- src/app/components/books/books.component.ts
+++ src/app/components/books/books.component.ts
@@ -1,7 +1,8 @@
-import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
+import { AfterViewInit, Component, OnInit, ViewChild, OnDestroy } from '@angular/core';
+import { Subscription } from 'rxjs';
 // Material
 import { MatDialog } from '@angular/material/dialog';
-import { MatPaginator } from '@angular/material/paginator';
+import { MatPaginator, PageEvent } from '@angular/material/paginator';
 import { MatSort } from '@angular/material/sort';
 import { MatTableDataSource } from '@angular/material/table';
 // Interfaces
@@ -9,6 +10,7 @@ import { Books } from '../../interfaces/';
 // Services
 import { BooksService } from '../../services/books.service';
 import { BookModalComponent } from '../book-modal/book-modal.component';
+import { PaginationBooks } from '../../interfaces/pagination-books.interface';
 
 
 @Component({
@@ -16,21 +18,40 @@ import { BookModalComponent } from '../book-modal/book-modal.component';
   templateUrl: './books.component.html',
   styleUrls: ['./books.component.css']
 })
-export class BooksComponent implements OnInit, AfterViewInit {
+export class BooksComponent implements OnInit, AfterViewInit, OnDestroy {
 
+  subscription: Subscription = new Subscription();
+
+  /* Set Sort and Pagination */
   @ViewChild(MatSort) order!: MatSort;
   @ViewChild(MatPaginator) paginator!: MatPaginator;
 
-  dataSource = new MatTableDataSource();
+  dataSource = new MatTableDataSource<Books>();
 
-  bookData: Books[] = [];
-  displayedColumns: string[] = ['title', 'description', 'autor', 'price'];
+  totalBooks: number = 0;
+  librosPorPagina: number = 2;
+  paginaCombo: number[] = [1, 2, 5, 10];
+  paginaActual: number = 1;
+  sort: string = 'titulo';
+  sortDirection: string = 'asc';
+  filterValue = null;
 
+  displayedColumns: string[] = ['titulo', 'descripcion', 'autor', 'precio'];
 
-  constructor(private booksService: BooksService, private dialog: MatDialog) { }
+  /* Inyect Services */
+  constructor(private booksService: BooksService,
+              private bookService: BooksService,
+              private dialog: MatDialog) { }
 
+  /* Get initial backend data and set default pagination */
   ngOnInit(): void {
     this.dataSource.data = this.booksService.books;
+    this.getDataPagination();
+  }
+
+  /* Destroy subscription */
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
   }
 
   ngAfterViewInit(): void {
@@ -38,14 +59,44 @@ export class BooksComponent implements OnInit, AfterViewInit {
     this.dataSource.paginator = this.paginator;
   }
 
+  /* Filter Data with DataSource Table */
   searchFilter(_event: Event): void{
     this.dataSource.filter = (_event.currentTarget as HTMLInputElement).value;
   }
 
-  openDialog(){
-    this.dialog.open(BookModalComponent,{
+  /* Update Modal */
+  openDialog(): void{
+    const dialogRef = this.dialog.open(BookModalComponent,{
       width: '350px'
     });
+
+    dialogRef.afterClosed()
+      .subscribe( () => {
+
+        this.getDataPagination();
+      })
+  }
+
+  /* Get Data with events table */
+  eventPagination(event: PageEvent): void{
+    this.librosPorPagina = event.pageSize;
+    this.paginaActual = event.pageIndex + 1;
+    this.getDataPagination();
+  }
+
+  /* Update Table with Pagination Backend */
+  getDataPagination(): void{
+    this.subscription.add(
+      this.booksService.getBooksPagination(this.librosPorPagina,
+        this.paginaActual,
+        this.sort,
+        this.sortDirection,
+        this.filterValue)
+          .subscribe( (pagination: PaginationBooks) => {
+            this.dataSource = new MatTableDataSource<Books>(pagination.data);
+            this.totalBooks = pagination.totalRows;
+          })
+    )
   }
 
 }
