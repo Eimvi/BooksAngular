43ca0548724e8435602c23a5d6bec9e03679ffb7
Cristopher <cristopher.emodesto@gmail.com>
2022-03-02 21:35:34 -0600

    Added post, get http methods

--- src/app/components/book-modal/book-modal.component.ts
+++ src/app/components/book-modal/book-modal.component.ts
@@ -1,24 +1,31 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
+import { Component, OnInit, ViewChild, OnDestroy } from '@angular/core';
 import {
   FormGroup,
   FormBuilder,
-  Validators,
-  AbstractControl
+  Validators
 } from "@angular/forms";
 
+import { Subscription } from 'rxjs';
+// Material
 import { MatOption } from '@angular/material/core';
 import { MatDatepicker } from '@angular/material/datepicker';
 import { MatSelectChange } from '@angular/material/select';
+// Service
 import { BooksService } from '../../services/books.service';
-import { Autor } from '../../interfaces/autor.interface';
 import { AutorsService } from '../../services/autors.service';
+// Interface
+import { Autor } from '../../interfaces/autor.interface';
+import { Books } from '../../interfaces/books.interface';
+
 
 @Component({
   selector: 'app-book-modal',
   templateUrl: './book-modal.component.html',
   styleUrls: ['./book-modal.component.css']
 })
-export class BookModalComponent implements OnInit {
+export class BookModalComponent implements OnInit, OnDestroy {
+
+  subscription: Subscription = new Subscription();
 
   @ViewChild(MatDatepicker) picker!: MatDatepicker<Date>;
   bookForm!: FormGroup;
@@ -33,27 +40,74 @@ export class BookModalComponent implements OnInit {
 
   ngOnInit(): void {
     this.createForm();
-    this.autors =  this.autorsService.authors;
+    this.getAutors();
   }
 
-
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
+  }
 
   createForm() {
     this.bookForm = this._fb.group({
-      title: ['', Validators.required],
-      description: ['', Validators.required],
-      price: ['', Validators.required],
+      titulo: ['', Validators.required],
+      descripcion: ['', Validators.required],
+      precio: ['', Validators.required],
       autor: ['', Validators.required],
-      dateCreated: [''],
+      fechaPublicacion: [''],
     });
   }
 
   saveBook(){
-    console.log('ejecutado');
+
+    const autor = {
+      id: this.autor?.value
+    }
+
+    const book: Books = {
+      titulo: this.titulo?.value,
+      descripcion: this.descripcion?.value,
+      precio: this.precio?.value,
+      autor: autor,
+      fechaPublicacion: this.fechaPublicacion?.value
+    }
+
+    this.subscription.add(
+      this.booksService.postBook(book)
+        .subscribe(() => {})
+    )
   }
 
   selected(event: MatSelectChange){
     this.selectAutorTxt = (event.source.selected as MatOption).viewValue;
+  }
+
+  getAutors(): void{
+    this.subscription.add(
+      this.autorsService.getAutors()
+        .subscribe( (autors) => {
+          this.autors = autors;
+        } )
+    )
+  }
+
+  get titulo(){
+    return this.bookForm.get('titulo');
+  }
+
+  get descripcion(){
+    return this.bookForm.get('descripcion');
+  }
+
+  get precio(){
+    return this.bookForm.get('precio');
+  }
+
+  get autor(){
+    return this.bookForm.get('autor');
+  }
+
+  get fechaPublicacion(){
+    return this.bookForm.get('fechaPublicacion');
   }
 
 }
