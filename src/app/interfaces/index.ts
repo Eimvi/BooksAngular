4095b67f9b86e13f988ee4425a8cd857ebf5a8cc
Cristopher <cristopher.emodesto@gmail.com>
2022-03-02 21:32:12 -0600

    Updated export interfaces

--- src/app/interfaces/index.ts
+++ src/app/interfaces/index.ts
@@ -2,3 +2,5 @@ export { Books } from "./books.interface";
 export { LoginData } from './login-data.interface';
 export { User } from './usuario.interface';
 export { Autor } from './autor.interface';
+export { PaginationBooks } from "./pagination-books.interface";
+
