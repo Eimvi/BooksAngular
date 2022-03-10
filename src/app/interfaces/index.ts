4724d88f8f08f7d477e82ca7ab0521c6ebe9213e
Cristopher <cristopher.emodesto@gmail.com>
2022-03-09 22:59:26 -0600

    Updated index export interfaces

--- src/app/interfaces/index.ts
+++ src/app/interfaces/index.ts
@@ -3,4 +3,5 @@ export { LoginData } from './login-data.interface';
 export { User } from './usuario.interface';
 export { Autor } from './autor.interface';
 export { PaginationBooks } from "./pagination-books.interface";
-
+export { PaginationAutors } from './pagination-autors.interface';
+export { UserLoginResponse } from "./user-login-response";
